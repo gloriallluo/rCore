d10aa821592cdd33994fceb4a797a8b3927f9569
罗境佳 <luojj18@mails.tsinghua.edu.cn>
2021-03-08 23:10:13 +0800

    add log

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,6 +4,8 @@
 #![feature(global_asm)]
 #![feature(panic_info_message)]     // 获取 panic 信息并打印
 
+use core::env;
+
 #[macro_use]
 mod console;
 mod lang_items;
@@ -37,11 +39,22 @@ pub extern "C" fn rust_main() {
         fn boot_stack_top();
     }
     clear_bss();
-    trace!("Hello, world!");
-    info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    warn!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    error!("boot_stack [{:#x}, {:#x})", boot_stack as usize, boot_stack_top as usize);
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
+    let log: &'static str = env!("LOG");
+    if log == "INFO" {
+        info!("Hello, world!");
+        info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+        info!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+        info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+        info!("boot_stack [{:#x}, {:#x})", boot_stack as usize, boot_stack_top as usize);
+        info!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    } else {
+        trace!("Hello, world!");
+        info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+        debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+        warn!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+        error!("boot_stack [{:#x}, {:#x})", boot_stack as usize, boot_stack_top as usize);
+        println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    }
     panic!("Shutdown machine!");
 }
